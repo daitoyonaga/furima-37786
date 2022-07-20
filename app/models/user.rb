1e31d9c41a01241e0b5009691d7d823a9da5c124
daitoyonaga <dtoyon41@icloud.com>
2022-07-20 11:00:50 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :encrypted_password,
+   format: { with: VALID_PASSWORD_REGEX, }
+  validates :nickname, presence: true
+  validates :first_name,
+   format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+   presence: true
+ validates :last_name,
+   format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+   presence: true
+ validates :first_name_kana,
+   format: { with: /\A([ァ-ン]|ー)+\z/},
+   presence: true
+ validates :last_name_kana,
+   format: { with: /\A([ァ-ン]|ー)+\z/},
+   presence: true
+ validates :birth_date,
+   presence: true
 end
