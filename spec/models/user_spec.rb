f613e818b080693c573625cc081148684970cd56
daitoyonaga <dtoyon41@icloud.com>
2022-07-25 13:19:38 +0900

    Rubocopの設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -100,24 +100,24 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it 'last_nameが全角入力でなければ登録できない' do
-        @user.last_name = "ｱｲｳｴｵ"
+        @user.last_name = 'ｱｲｳｴｵ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name is invalid")
+        expect(@user.errors.full_messages).to include('Last name is invalid')
       end
       it 'first_nameが全角入力でなければ登録できない' do
-        @user.first_name = "ｱｲｳｴｵ"
+        @user.first_name = 'ｱｲｳｴｵ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name is invalid")
+        expect(@user.errors.full_messages).to include('First name is invalid')
       end
       it 'last_name_kanaが全角カタカナでなければ登録できない' do
-        @user.last_name_kana = "あいうえお" 
+        @user.last_name_kana = 'あいうえお'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana is invalid")
+        expect(@user.errors.full_messages).to include('Last name kana is invalid')
       end
       it 'first_name_kanaが全角カタカナでなければ登録できない' do
-        @user.first_name_kana = "あいうえお"
+        @user.first_name_kana = 'あいうえお'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name kana is invalid")
+        expect(@user.errors.full_messages).to include('First name kana is invalid')
       end
     end
   end
