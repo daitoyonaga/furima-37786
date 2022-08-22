ea3aa5f6deb55bbef83318b2365b1acdf7ba7d51
daitoyonaga <dtoyon41@icloud.com>
2022-08-22 14:11:51 +0900

    pay_itemメソッドの定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
  def create
   @order_form = OrderForm.new(order_params)
   if @order_form.valid?
+    pay_item
     @order_form.save
     redirect_to root_path
   else
@@ -20,6 +21,15 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,        # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy'             # 通貨の種類（日本円）
+    )
   end
 
   def non_purchased_item
