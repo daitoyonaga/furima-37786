2afbf0d55a316bdb9394f4c467245bf44c7132f0
daitoyonaga <dtoyon41@icloud.com>
2022-08-22 12:13:45 +0900

    バリデーションの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,15 +9,14 @@ class OrdersController < ApplicationController
  def create
   @order_form = OrderForm.new(order_params)
   if @order_form.valid?
-    pay_item
     @order_form.save
     redirect_to root_path
   else
     render :index
   end
-end
+ end
 
-private
+ private
 
   def order_params
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
@@ -27,5 +26,4 @@ private
     @item = Item.find(params[:item_id])
     redirect_to root_path if current_user.id == @item.user_id
   end
-
 end
