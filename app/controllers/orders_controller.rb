420947d88321d02bb48a756bb95b7474cc40dafc
daitoyonaga <dtoyon41@icloud.com>
2022-08-23 23:46:39 +0900

    Rubocopを実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,28 @@
 class OrdersController < ApplicationController
- before_action :authenticate_user!
- before_action :non_purchased_item, only: [:index, :create]
+  before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
 
- def index
-  @order_form = OrderForm.new
- end
-
- def create
-  @order_form = OrderForm.new(order_params)
-  if @order_form.valid?
-    pay_item
-    @order_form.save
-    redirect_to root_path
-  else
-    render :index
+  def index
+    @order_form = OrderForm.new
   end
- end
 
- private
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
 
   def order_params
-    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :address, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def pay_item
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -6,7 +6,7 @@ class OrderForm
     validates :user_id
     validates :item_id
 
-    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)'}
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
@@ -16,6 +16,7 @@ class OrderForm
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    ShippingInformation.create(order_id: order.id, postcode: postcode, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
+    ShippingInformation.create(order_id: order.id, postcode: postcode, prefecture_id: prefecture_id, city: city,
+                               address: address, building_name: building_name, phone_number: phone_number)
   end
 end
